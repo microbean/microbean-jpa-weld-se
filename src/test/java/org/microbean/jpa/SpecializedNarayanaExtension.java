a8b16eaa476ea5fc0818088978718bb73670f806
Laird Nelson <ljnelson@gmail.com>
2018-07-12 12:22:48 -0700

    Continuing to hack Narayana
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

--- src/test/java/org/microbean/jpa/SpecializedNarayanaExtension.java
+++ src/test/java/org/microbean/jpa/SpecializedNarayanaExtension.java
@@ -22,6 +22,8 @@ import java.lang.reflect.Type;
 
 import java.util.Set;
 
+import javax.enterprise.context.Dependent;
+
 import javax.enterprise.event.Observes;
 
 import javax.enterprise.inject.spi.AfterBeanDiscovery;
@@ -32,9 +34,10 @@ import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
 
-import javax.inject.Singleton;
-
 import javax.transaction.TransactionManager;
+import javax.transaction.TransactionSynchronizationRegistry;
+
+import com.arjuna.ats.internal.jta.transaction.arjunacore.TransactionSynchronizationRegistryImple;
 
 import com.arjuna.ats.jta.cdi.TransactionContext;
 
@@ -45,20 +48,36 @@ public final class SpecializedNarayanaExtension implements Extension {
   }
 
   private final void beforeBeanDiscovery(@Observes final BeforeBeanDiscovery event) throws ReflectiveOperationException {
-    final Field field = TransactionContext.class.getDeclaredField("transactionManager");
+
+    // Hack the TransactionContext class to not require JNDI.
+    
+    Field field = TransactionContext.class.getDeclaredField("transactionManager");
     assert field != null;
-    final int modifiers = field.getModifiers();
-    assert Modifier.isStatic(modifiers);
-    assert Modifier.isPrivate(modifiers);
+    assert Modifier.isStatic(field.getModifiers());
+    assert Modifier.isPrivate(field.getModifiers());
     assert TransactionManager.class.equals(field.getType());
     field.setAccessible(true);
     field.set(null, com.arjuna.ats.jta.TransactionManager.transactionManager());
+
+    field = TransactionContext.class.getDeclaredField("transactionSynchronizationRegistry");
+    assert field != null;
+    assert Modifier.isStatic(field.getModifiers());
+    assert Modifier.isPrivate(field.getModifiers());
+    assert TransactionSynchronizationRegistry.class.equals(field.getType());
+    field.setAccessible(true);
+    field.set(null, new TransactionSynchronizationRegistryImple());
   }
   
   private final void afterBeanDiscovery(@Observes final AfterBeanDiscovery event) {
+    // A note on the scope for this bean:
+    // com.arjuna.ats.jta.TransactionManager#transactionManager()
+    // returns a singleton.  But I suppose there is no guarantee in
+    // the future that it will do so, and in any event the rest of the
+    // Narayana infrastructure doesn't always cache the return value
+    // of this method, so we want to preserve those semantics.
     event.addBean()
       .types(TransactionManager.class)
-      .scope(Singleton.class)
+      .scope(Dependent.class)
       .createWith(cc -> com.arjuna.ats.jta.TransactionManager.transactionManager());
   }
 
@@ -71,6 +90,13 @@ public final class SpecializedNarayanaExtension implements Extension {
         final String name = baseType.getTypeName();
         assert name != null;
         if (name.startsWith("com.arjuna.ats.jta.cdi.transactional.TransactionalInterceptor")) {
+          // Programmatically remove the interceptors installed by the
+          // stock Narayana extension.  These will trigger a JNDI
+          // lookup which will fail, since we are not guaranteed the
+          // presence of a JNDI implementation in the microBean
+          // environment.  We replace them with similar almost-clones
+          // present in this package that bypass the JNDI machinery.
+          // See also: https://github.com/jbosstm/narayana/pull/1344
           event.veto();
         }
       }
